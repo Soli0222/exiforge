a0ebf00a04ae1fddc7c951873c8af0ca37f0a478
Soli <github@str08.net>
2025-03-08 00:18:05 +0900

    refactoring (#3)
    * enhance(version): improve version display format and structure
    
    * refactor: restructure file processing and EXIF extraction logic

--- cmd/root.go
+++ cmd/root.go
@@ -6,12 +6,11 @@ package cmd
 import (
 	"log"
 	"os"
-	"path/filepath"
 	"strings"
-	"time"
 
-	"github.com/rwcarlsen/goexif/exif"
-	"github.com/schollz/progressbar/v3"
+	"exiforge/internal/exif"
+	"exiforge/internal/processor"
+
 	"github.com/spf13/cobra"
 )
 
@@ -25,13 +24,25 @@ var rootCmd = &cobra.Command{
 	Use:   "exiforge",
 	Short: "Organize pictures using EXIF data",
 	Long:  `Organize picture files by date and optionally by camera model based on EXIF metadata.`,
-	Run: func(cmd *cobra.Command, args []string) {
+	RunE: func(cmd *cobra.Command, args []string) error {
+		extractor := exif.NewExtractor()
+		fileProcessor := processor.NewFileProcessor(extractor, log.Default())
+
 		extensions := strings.Split(ext, ",")
 		for _, e := range extensions {
 			upperExt := strings.ToUpper(strings.TrimSpace(e))
 			pattern := "*." + upperExt
-			processFiles(dir, pattern, upperExt, model)
+
+			opts := processor.ProcessorOptions{
+				UseModel: model,
+			}
+
+			if err := fileProcessor.ProcessFiles(dir, pattern, upperExt, opts); err != nil {
+				return err
+			}
 		}
+
+		return nil
 	},
 }
 
@@ -48,125 +59,4 @@ func init() {
 	rootCmd.Flags().StringVarP(&ext, "extensions", "e", "JPG,ARW", "Comma-separated list of file extensions to process (e.g., JPG,DNG,PNG)")
 	rootCmd.Flags().StringVarP(&dir, "directory", "d", ".", "Directory to process")
 	rootCmd.Flags().BoolVarP(&model, "model", "m", false, "Use camera model from EXIF for file organization")
-	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
-}
-
-func processFiles(baseDir, pattern, ext string, useModel bool) {
-	files, err := filepath.Glob(filepath.Join(baseDir, pattern))
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	if len(files) == 0 {
-		log.Printf("No files found for pattern %s in %s", pattern, baseDir)
-		return
-	}
-
-	// Determine the output directory.
-	var outputDir string
-	if useModel {
-		outputDir = baseDir
-	} else {
-		outputDir = filepath.Join(baseDir, ext)
-		if err := os.Mkdir(outputDir, 0755); err != nil && !os.IsExist(err) {
-			log.Fatal(err)
-		}
-	}
-
-	bar := progressbar.NewOptions(
-		len(files),
-		progressbar.OptionSetDescription("Processing "+ext),
-	)
-
-	for _, file := range files {
-		var subDir string
-		if useModel {
-			// Folder structure: ${Model}/${Extension}/${Date}
-			modelStr, err := extractModel(file)
-			if err != nil {
-				log.Printf("Error extracting model from %s: %v", file, err)
-				bar.Add(1)
-				continue
-			}
-			dateStr, err := extractDate(file)
-			if err != nil {
-				log.Printf("Error extracting date from %s: %v", file, err)
-				bar.Add(1)
-				continue
-			}
-			subDir = filepath.Join(modelStr, ext, dateStr)
-		} else {
-			subDir, err = extractDate(file)
-			if err != nil {
-				log.Printf("Error processing %s: %v", file, err)
-				bar.Add(1)
-				continue
-			}
-		}
-
-		destDir := filepath.Join(outputDir, subDir)
-		if err := os.MkdirAll(destDir, 0755); err != nil {
-			log.Fatal(err)
-		}
-
-		destPath := filepath.Join(destDir, filepath.Base(file))
-		if err := moveFile(file, destPath); err != nil {
-			log.Printf("Failed to move %s: %v", file, err)
-		}
-
-		bar.Add(1)
-	}
-	println()
-}
-
-func extractDate(filename string) (string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	exifData, err := exif.Decode(file)
-	if err != nil {
-		return "", err
-	}
-
-	dateTime, err := exifData.Get(exif.DateTimeOriginal)
-	if err != nil {
-		return "", err
-	}
-
-	dateStr := strings.Trim(dateTime.String(), "\"")
-	parsedTime, err := time.Parse("2006:01:02 15:04:05", dateStr)
-	if err != nil {
-		return "", err
-	}
-
-	return parsedTime.Format("2006-01-02"), nil
-}
-
-func extractModel(filename string) (string, error) {
-	file, err := os.Open(filename)
-	if err != nil {
-		return "", err
-	}
-	defer file.Close()
-
-	exifData, err := exif.Decode(file)
-	if err != nil {
-		return "", err
-	}
-
-	modelTag, err := exifData.Get(exif.Model)
-	if err != nil {
-		return "", err
-	}
-
-	modelStr := strings.Trim(modelTag.String(), "\"")
-	modelStr = strings.ReplaceAll(modelStr, " ", "_")
-	return modelStr, nil
-}
-
-func moveFile(src, dst string) error {
-	return os.Rename(src, dst)
 }
