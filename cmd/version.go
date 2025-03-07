4abf08d65a1789c91233715efbaaa3a84b3beff5
Soli <github@str08.net>
2025-03-07 22:52:44 +0900

    Replase cobra (#1)
    * feat: restructure code to use Cobra for command-line interface and add version command
    
    * fix: update copyright year in LICENSE file to 2025
    
    * feat: rename project from Picture Arrange to Exiforge and update related files
    
    * fix: update copyright information to reflect current ownership
    
    * enhance: update GitHub Actions and GoReleaser configuration for Homebrew integration

---
+++ cmd/version.go
@@ -0,0 +1,35 @@
+/*
+Copyright © 2025 Soli
+*/
+package cmd
+
+import (
+	"fmt"
+	"runtime"
+
+	"github.com/spf13/cobra"
+)
+
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "Display the version of exiforge",
+	Long:  "This command displays the current version of the exiforge CLI tool.",
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("exiforge version v2.0.0", runtime.GOARCH)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(versionCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// versionCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// versionCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
